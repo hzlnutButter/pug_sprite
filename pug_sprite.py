b829f5ba35f6bcf3630203b7562f33a5811517e6
hzlnutButter <77945587+hzlnutButter@users.noreply.github.com>
2021-02-15 16:28:53 -0800

    pug_sprite2
    uses classes to create animations. includes a pug and a hedgehog.

--- pug_sprite.py
+++ pug_sprite.py
@@ -1,73 +1,147 @@
 import time
 from os import system
 
-pug1 = [
-"                         ________",
-"    ___                 /\  /    \\",
-"   /   \               /  \/  O __\\",
-"  |  \_/              /       /   |",
-"   \_________________/        |__/",
-"   |                          /",
-"   |                         /",
-"   |                        /",
-"   |   /                   /",
-"   |  /\_______________/| |",
-"   | |                  | |",
-"   |_|                  |_|"]
+class Shape():
+    def __init__(self, lines):
+        self.lines = lines
+    def add_spaces(self, spaces):
+        new_lines = []
+        for line in self.lines:
+            new_lines.append(" " * spaces + line)
+        greater_than_70 = False
+        for line in new_lines:
+            if len(line) > 70:
+                greater_than_70 = True
+        self.lines = new_lines
+        return greater_than_70
+    def remove_spaces(self, spaces):
+        new_lines = []
+        for line in self.lines:
+            if line[:spaces] == " " * spaces:
+                new_lines.append(line[spaces:])
+        self.lines = new_lines
+    def print_shape(self):
+        for line in self.lines:
+            print(line)
 
-pug2 = [
-"                         ________",
-"    ___                 /\  /    \\",
-"   /   \               /  \/  O __\\",
-"  |  \_/              /       /   |",
-"   \_________________/        |__/",
-"   |                          /",
-"   |                         /",
-"   |                        /",
-"   |    /                  /",
-"    \  /\_______________/\ \\",
-"     \ \\                  \ \\",
-"      \_\\                  \_\\"]
+class Sprite():
+    def __init__(self, shapes):
+        self.shapes = shapes
+    def change_all(self, amount_to_change):
+        greater_than_70 = False
+        for shape in self.shapes:
+            if amount_to_change > 0:
+                greater_than_70 = shape.add_spaces(amount_to_change)
+            elif amount_to_change < 0:
+                shape.remove_spaces(amount_to_change)
+        return greater_than_70
+    def cycle_print_shapes(self, list_of_indices, sleep_time):
+        for index in list_of_indices:
+            system("clear")
+            self.shapes[index].print_shape()
+            time.sleep(sleep_time)
 
-pug3 = [
-"                         ________",
-"    ___                 /\  /    \\",
-"   /   \               /  \/  O __\\",
-"  |  \_/              /       /   |",
-"   \_________________/        |__/",
-"   |                          /",
-"   |                         /",
-"   |                        /",
-"   |     /                 /",
-"   / ___/\______________/ /",
-"  / /                  / /",
-" /_/                  /_/"]
+if True: # define shapes
 
-def print_sprite(sprite, spaces):
-    new_sprite = []
-    for line in sprite:
-        new_sprite.append((" " * spaces) + line)
-    for line in new_sprite:
-        print(line)
-    time.sleep(.1)
-    system("clear")
-    if len(new_sprite[3]) >=70:
-        return
-    return new_sprite
+    pug1 = [
+    "                  _______",
+    "    ___          /\  /   \\",
+    "   /   \        /  \/ O __\\",
+    "  |  \_/       /      /   |",
+    "   \__________/       |__/",
+    "   |                  /",
+    "   |                 /",
+    "   |   /            /",
+    "   |  /\________/| |",
+    "   | |           | |",
+    "   |_|           |_|"]
 
-def walk_LR(pug1, pug2, pug3):
+    pug2 = [
+    "   __             _______",
+    " /    \          / \ |   \\",
+    "|      |        /   \|- __\\",
+    "|   \_/        /      /   |",
+    " \____________/       |__/",
+    "  \                   /",
+    "  |                  /",
+    "  |    /            /",
+    "   \  /\________/\ \\",
+    "    \ \\          \ \\",
+    "     \_\\          \_\\"]
+
+    pug3 = [
+    "                  _______",
+    "                 /| /    \\",
+    "       __       / |/  O __\\",
+    "     / _/      /      /   |",
+    "    |_________/       |__/",
+    "    /                 /",
+    "   |                 /",
+    "   |   /            /",
+    "   / _/\_________/ /",
+    "  / /           / /",
+    " /_/           /_/"]
+
+    hog1 = [
+    "",
+    "      \\\\\\\\\\\\\\\\",
+    "     \\\\\\\\\\\\\\ o",
+    "    \\\\\\\\\\\\\\\\   >",
+    "      |     |"]
+
+    hog2 = [
+    "",
+    "      \\\\\\\\\\\\\\\\",
+    "     \\\\\\\\\\\\\\ -",
+    "    \\\\\\\\\\\\\\\\   >",
+    "      \\     \\"]
+
+    hog3 = [
+    "",
+    "      \\\\\\\\\\\\\\\\",
+    "     \\\\\\\\\\\\\\ o",
+    "    \\\\\\\\\\\\\\\\   >",
+    "      /     /"]
+
+    hog4 = [
+    "",
+    "        \\\\|//",
+    "      \\\\\\\\|////",
+    "     \\\\\\ - - ///",
+    "    \\\\\\\\  V  ////"]
+
+    hog5 = [
+    "",
+    "      \\ \\ | / /",
+    "  \\ \\ \\ \\ | / / / /",
+    "  \\ \\ \\  v v  / / /",
+    "\\ \\ \\ \\   V   / / / /"]
+
+pug_shape1 = Shape(pug1)
+pug_shape2 = Shape(pug2)
+pug_shape3 = Shape(pug3)
+pug = Sprite([pug_shape1, pug_shape2, pug_shape3])
+
+def make_pug_walk():
     while True:
-        pug1 = print_sprite(pug1, 2)
-        if pug1 is None:
-            break
-        pug2 = print_sprite(pug2, 4)
-        if pug2 is None:
-            break
-        pug1 = print_sprite(pug1, 2)
-        if pug1 is None:
-            break
-        pug3 = return_val = print_sprite(pug3, 4)
-        if pug3 is None:
+        pug.cycle_print_shapes([0, 1, 0, 2], .1)
+        if pug.change_all(3):
             break
 
-walk_LR(pug1, pug2, pug3)
+hog_shape1 = Shape(hog1)
+hog_shape2 = Shape(hog2)
+hog_shape3 = Shape(hog3)
+hog_shape4 = Shape(hog4)
+hog_shape5 = Shape(hog5)
+hedgehog = Sprite([hog_shape1, hog_shape2, hog_shape3, hog_shape4, hog_shape5])
+
+def make_hog_walk():
+    while True:
+        hedgehog.cycle_print_shapes([0, 1, 0, 2], .1)
+        if hedgehog.change_all(3):
+            break
+    hedgehog.cycle_print_shapes([3, 4, 3, 4], .2)
+    print(" " * 60 + "huff")
+
+make_hog_walk()
+make_pug_walk()
